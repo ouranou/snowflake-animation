0588dc9ce673621ddbaf233b8ce3f06f1a1052d7
linhaoran <546344877@qq.com>
2016-10-28 16:59:05 +0800

    d
    dded

--- snow/ViewController.m
+++ snow/ViewController.m
@@ -20,7 +20,7 @@
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor blackColor];
     NSLog(@"ssssssssssssssssss");
-    NSLog(@"dddddddddddddddddd");
+    NSLog(@"ddddddddddddd");
     // 创建粒子Layer
     CAEmitterLayer *snowEmitter = [CAEmitterLayer layer];
     // 粒子发射位置
